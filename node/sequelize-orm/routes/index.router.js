5b247a09604b159e37d55e06ce789b28be0081d9
Tanner <thetannerthomas@gmail.com>
2021-07-24 17:48:20 -0600

    initial commit

---
+++ node/sequelize-orm/routes/index.router.js
@@ -0,0 +1,15 @@
+import express from "express";
+import db from "../database/db";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const usr = await db.create("Joe", "Smith", "This is my bio");
+    res.json(usr);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+export default router;
