9ddf49c52723f658be4ae9220f55ea9d883d94f3
Tanner <thetannerthomas@gmail.com>
2021-07-26 13:08:12 -0600

    update end points

--- node/sequelize-orm/routes/api.router.js
+++ node/sequelize-orm/routes/api.router.js
@@ -3,7 +3,26 @@ import db from "../database/db";
 
 const router = express.Router();
 
-/* Get */
+/**
+ * GET
+ *
+ * @ /api/find - returns 1 user based on the id provided
+ *
+ * @ /api/findall - returns all users
+ *
+ * @ /api/filter - returns users based on criteria
+ */
+router.get("/find", async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    const user = await db.findUser(id);
+    res.json(user);
+  } catch (error) {
+    console.error(err);
+    res.status(404).send(error);
+  }
+});
 router.get("/findall", async (req, res) => {
   try {
     const users = await db.findAllUsers();
@@ -13,8 +32,23 @@ router.get("/findall", async (req, res) => {
     res.status(404).send(error);
   }
 });
+router.get("/filter", async (req, res) => {
+  //// const { name } = req.query;
 
-/* Post */
+  try {
+    const users = await db.filterUsers();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(404).send(error);
+  }
+});
+
+/**
+ * POST
+ *
+ * @ /api/post - creates a new user
+ */
 router.post("/post", async (req, res) => {
   const newUser = req.body.user;
 
@@ -26,7 +60,24 @@ router.post("/post", async (req, res) => {
     });
   } catch (err) {
     console.error(err);
-    res.status(404).send(error);
+    res.status(404).send(err);
+  }
+});
+
+/**
+ * PUT
+ *
+ * @ /api/update - updates a users name and password based on id provided
+ */
+router.put("/update", async (req, res) => {
+  const { name, password, id } = req.body;
+
+  try {
+    await db.updateUser(name, password, id);
+    res.status(200).send("success");
+  } catch (err) {
+    console.error(err);
+    res.status(404).send(err);
   }
 });
 
