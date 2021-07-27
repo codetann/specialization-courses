45ef3d3a2db6a9b78ae5b6bc091427cc289805b6
Tanner <thetannerthomas@gmail.com>
2021-07-27 10:31:43 -0600

    create courses path

--- react/react-redux-router/src/components/App.js
+++ react/react-redux-router/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+// pages
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import CoursesPage from "./courses/CoursesPage";
 import PageNotFound from "./PageNotFound";
+// components
 import Header from "./common/Header";
 
 export default function App() {
@@ -15,6 +17,7 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/about" component={AboutPage} />
+          <Route path="/courses" component={CoursesPage} />
           <Route component={PageNotFound} />
         </Switch>
       </div>
