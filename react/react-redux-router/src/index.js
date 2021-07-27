2657f83935637ad367ce1656e9d33c5843f0d955
Tanner <thetannerthomas@gmail.com>
2021-07-27 09:56:21 -0600

    include App.js

--- react/react-redux-router/src/index.js
+++ react/react-redux-router/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const Test = () => {
-  return <p>Hi</p>;
-};
-
-ReactDOM.render(<Test />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
