dd53743c248127472ea3ad6155792cd8cdcf66dc
Tanner <thetannerthomas@gmail.com>
2021-07-27 10:30:33 -0600

    refactor links

--- react/react-redux-router/src/components/common/Header.jsx
+++ react/react-redux-router/src/components/common/Header.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const LINKS = [
+  { to: "/", link: "Home" },
+  { to: "/about", link: "About" },
+  { to: "/courses", link: "Courses" },
+];
+
 export default function Header() {
   const activeStyle = { color: "#F15B2A" };
+  const style = { marginRight: "1rem" };
 
   return (
     <div>
-      <NavLink to="/" activeStyle={activeStyle} exact>
-        Home
-      </NavLink>
-      {" | "}
-      <NavLink to="/about" activeStyle={activeStyle}>
-        About
-      </NavLink>
+      {LINKS.map((link, i) => (
+        <NavLink key={i} style={style} activeStyle={activeStyle} to={link.to}>
+          {link.link}
+        </NavLink>
+      ))}
     </div>
   );
 }
