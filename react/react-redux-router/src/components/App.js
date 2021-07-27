764f726a3b668c687eddbbd51ea4d152dafb8e45
Tanner <thetannerthomas@gmail.com>
2021-07-27 10:18:08 -0600

    import Header

--- react/react-redux-router/src/components/App.js
+++ react/react-redux-router/src/components/App.js
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
 
 export default function App() {
   return (
     <Router>
       <div className="container-fluid">
+        <Header />
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
       </div>
