cfcae1180ed771b4ff4a9005b9597b0f7f9e812e
Tanner <thetannerthomas@gmail.com>
2021-07-28 09:33:34 -0600

    add form

--- react/react-redux-router/src/components/courses/CoursesPage.jsx
+++ react/react-redux-router/src/components/courses/CoursesPage.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CoursesPage() {
+  const [title, setTitle] = useState({ course: "" });
+
+  // Event Functions
+  const handle = {
+    change: (e) => {
+      setTitle((prevState) => {
+        return { ...prevState, course: e.target.value };
+      });
+    },
+    submit: (e) => {
+      e.preventDefault();
+      setTitle({ course: "" });
+    },
+  };
+
   return (
     <div>
-      <h2>Courses</h2>
+      <form onSubmit={handle.submit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input type="text" onChange={handle.change} value={title.course} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
