73a2225f1298a4e53dcfc7e4bc4677adcf82e75f
Tanner <thetannerthomas@gmail.com>
2021-07-28 09:37:32 -0600

    config redux provider

--- react/react-redux-router/src/index.js
+++ react/react-redux-router/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import configStore from "./redux/config.store";
+import { Provider as ReduxProvider } from "react-redux";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const store = configStore();
+
+ReactDOM.render(
+  <ReduxProvider store={store}>
+    <App />
+  </ReduxProvider>,
+  document.getElementById("app")
+);
